655f09c08562617770c2c3cf74c6fb3e9b6ed3a7
bentona <benton.anderson@gmail.com>
2016-11-12 17:04:24 -0800

    works again

--- canvas.js
+++ canvas.js
@@ -5,11 +5,20 @@ let dimX = 1440;
 let dimY = 900;
 let offset = 0;
 
+
+
 random = (n) => Math.floor(Math.random() * n)
 
-distance = (a, b) => {
+euclideanMetric = (a, b) => {
+  return Math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+}
+
+taxicabMetric = (a, b) => {
   return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
-  //return Math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+}
+
+distance = (a, b) => {
+  return 0.2 * euclideanMetric(a, b) + 0.8 * taxicabMetric(a, b)
 }
 
 range = (n) => [...Array(n).keys()]
@@ -45,6 +54,10 @@ class Color{
 }
 
 randomColor = (bias) => {
+  return new Color(random(255), random(255), random(255));
+}
+
+blueRedColor = (bias) => {
   let flip = Math.floor(random(2))
   let randComp = random(bias * 100)
   let progComp = bias * 155;
@@ -63,22 +76,27 @@ colorPoint = (x, y, size, color) => {
 doit = (n) => {
   let points = range(n).map((x) => randomPoint())
   let results = cache(distance, points)
-  console.log(results)
-  results.map((xs) => xs.map((ys) => colorPoint(ys[0].x, ys[0].y, ys[1].color)))
-}
-
-pixerate = (x, y, fcn) => {
-  return range(x).map( (x1) => {
-    return range(y).map( (y1) => {
-      return fcn(x1, y1)
+  results.map((xs) => {
+    xs.map((ys) => {
+      return colorPoint(ys[0].x, ys[0].y, 1, ys[1].color)
     })
   })
 }
 
+pixerate = (x, y, fcn) => {
+  return range(x).map( (x1) => {
+    let row = range(y).map( (y1) => {
+      return fcn(x1, y1)
+    })
+    return row
+  })
+}
+
 cache = (metric, points) => {
-  pixerate(dimX, dimY, (x, y) => {
+  return pixerate(dimX, dimY, (x, y) => {
     let pt = new Point(x, y, false);
-    return [pt, closest(pt, points, metric)]
+    let nearest = closest(pt, points, metric)
+    return [pt, nearest]
   })
 }
 
